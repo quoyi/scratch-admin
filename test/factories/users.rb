baff3c89fb56cb5020035711cad45e8901f410fe
Quo yi <quoyi007@gmail.com>
2021-01-16 15:52:10 +0800

    update annotate config to not sort column

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -4,16 +4,16 @@
 #
 # Table name: users
 #
-#  current_sign_in_at     :datetime
-#  current_sign_in_ip     :string
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  last_sign_in_at        :datetime
-#  last_sign_in_ip        :string
-#  remember_created_at    :datetime
-#  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
 #  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
 #
 # Indexes
 #

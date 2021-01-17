e9cad6e37a4096d25c37068c1765c1f75e4cc359
Quo yi <quoyi007@gmail.com>
2021-01-17 12:03:31 +0800

    add settings seed data

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -16,6 +16,8 @@
 class Setting < RailsSettings::Base
   cache_prefix { 'v1' }
 
+  scope :presets, -> { where(preset: true) }
+
   # Define your fields
   # field :host, type: :string, default: "http://localhost:3000"
   # field :default_locale, default: "en", type: :string
@@ -23,4 +25,10 @@ class Setting < RailsSettings::Base
   # field :admin_emails, default: "admin@rubyonrails.org", type: :array
   # field :omniauth_google_client_id, default: ENV["OMNIAUTH_GOOGLE_CLIENT_ID"], type: :string, readonly: true
   # field :omniauth_google_client_secret, default: ENV["OMNIAUTH_GOOGLE_CLIENT_SECRET"], type: :string, readonly: true
+  field :app_name,         type: :string,  default: '慧玩慧学'
+  field :host,             type: :string,  default: 'jjerp.cn'
+  field :default_password, type: :string,  default: '123abc..'
+
+  field :registerable,     type: :boolean, default: false
+  field :confirmable,      type: :boolean, default: false
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Setting.keys.each { |key| Setting.create_with(value: Setting.send(key), preset: true).find_or_create_by(var: key) }
+
+User.create_with(password: '123abc..').find_or_create_by(email: 'admin@example.com')
+User.create_with(password: '123abc..').find_or_create_by(email: 'guest@example.com')

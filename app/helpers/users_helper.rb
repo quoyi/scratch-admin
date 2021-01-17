02a1604651da2d30f8994e350892aa2c67d88fd5
Quo yi <quoyi007@gmail.com>
2021-01-17 21:58:47 +0800

    add user helper methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def myself?(user)
+    user && current_user && current_user.id == user.id
+  end
+
+  # 谓语
+  def predicate(user)
+    myself?(user) ? '我' : '他'
+  end
 end

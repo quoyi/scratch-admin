466c09d8f933d78d5d9c524a1da32e698ab70e96
Quo yi <quoyi007@gmail.com>
2021-01-17 21:59:22 +0800

    add draw_tree helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # 表格树
+  def draw_tree(partial:, object:, **opts)
+    concat(render(partial, object: object, **opts)) unless object.superior
+
+    object.juniors.each do |junior|
+      next unless junior
+
+      concat(render(partial, object: junior, **opts))
+      draw_tree(partial: partial, object: junior, **opts) if junior.juniors
+    end
+  end
 end

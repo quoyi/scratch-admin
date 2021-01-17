2f94cfe583a7d76a2ba386aa101ace30916e8ad1
Quo yi <quoyi007@gmail.com>
2021-01-17 20:33:03 +0800

    update layouts & js & styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,24 +1,7 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import "../stylesheets/application";
-require.context("../images", true);
-
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
-
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
-
-import "@fortawesome/fontawesome-free/js/all";
-import * as bootstrap from "bootstrap";
-
-window.$ = window.jQuery = $;
-window.bootstrap = bootstrap;
-
 import "./bootstrap";
+
+// document.addEventListener("DOMContentLoaded", (e) => {
+document.addEventListener("turbolinks:load", (e) => {
+  window.dispatchEvent(new Event("resize"));
+});
--- app/javascript/packs/bootstrap.js
+++ app/javascript/packs/bootstrap.js
@@ -1,3 +1,20 @@
+require.context("../images", true);
+
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+import "@fortawesome/fontawesome-free/js/all";
+import * as bootstrap from "bootstrap";
+
+window.$ = window.jQuery = $;
+window.bootstrap = bootstrap;
+
 import { getAll } from "./utils";
 
 document.addEventListener("DOMContentLoaded", (e) => {

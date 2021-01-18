9df088d09c5a0d3c754244dbd0895488f25db60a
Quo yi <quoyi007@gmail.com>
2021-01-18 19:04:40 +0800

    update admin/roles test

--- test/controllers/admin/roles_controller_test.rb
+++ test/controllers/admin/roles_controller_test.rb
@@ -4,48 +4,52 @@ require_relative '../../test_helper'
 
 class Admin::RolesControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @admin = create(:admin)
+    login_as @admin
+
     @role = create(:role)
   end
 
   test 'should get index' do
-    get roles_url
+    get admin_roles_url
     assert_response :success
   end
 
   test 'should get new' do
-    get new_role_url
+    get new_admin_role_url
     assert_response :success
   end
 
   test 'should create role' do
+    @new_role = build(:role)
     assert_difference('Role.count') do
-      post roles_url, params: { role: @role.as_json }
+      post admin_roles_url, params: { role: @new_role.as_json }
     end
 
-    assert_redirected_to role_url(Role.last)
+    assert_redirected_to admin_roles_url
   end
 
   test 'should show role' do
-    get role_url(@role)
+    get admin_role_url(@role)
     assert_response :success
   end
 
   test 'should get edit' do
-    get edit_role_url(@role)
+    get edit_admin_role_url(@role)
     assert_response :success
   end
 
   test 'should update role' do
-    patch role_url(@role),
-          params: { role: @role.as_json }
-    assert_redirected_to role_url(@role)
+    @new_role = build(:role)
+    patch admin_role_url(@role), params: { role: @new_role.as_json }
+    assert_redirected_to admin_roles_url
   end
 
   test 'should destroy role' do
     assert_difference('Role.count', -1) do
-      delete role_url(@role)
+      delete admin_role_url(@role)
     end
 
-    assert_redirected_to roles_url
+    assert_redirected_to admin_roles_url
   end
 end

618545e9a257ce6f6d06f88e63cba059e842d77a
Quo yi <quoyi007@gmail.com>
2021-01-19 21:35:22 +0800

    add course create callback & test it

---
+++ app/models/concerns/stackable.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+module Stackable
+  extend ActiveSupport::Concern
+
+  included do
+    before_create do
+      self.seq = peers.count + 1
+      self.prev_id = peers.last.try(:id) if superior
+    end
+    # after_commit :build_stack
+    # before_create :build_stack
+    # before_destroy :rebuild_stack
+    # after_create_commit :update_seq
+    # after_destroy_commit :rebuild_seq
+
+    # rubocop:disable Rails/ReflectionClassName
+    belongs_to :prev, class_name: name, inverse_of: :after, optional: true
+    has_one :after, class_name: name, dependent: :nullify, foreign_key: 'prev_id', inverse_of: :prev
+    # rubocop:enable Rails/ReflectionClassName
+  end
+
+  # 同辈节点
+  def peers
+    self.class.where(superior_id: superior_id).order(:seq)
+  end
+
+  # 兄弟节点
+  def siblings
+    self.class.where('superior_id = ? AND id != ?', superior_id, id).order(:seq)
+  end
+
+  private
+
+    def insert_head
+      ActiveRecord::Base.transaction do
+        peers_length = peers.length
+        peers.reverse.each_with_index do |peer, index|
+          if peers_length == 1
+            peer.update(seq: 1)
+          else
+            peer_seq = peers_length + 1 - index
+            peer.update(seq: peer_seq, prev_id: peers[peers_length - 1].id)
+          end
+        end
+
+        # update(seq: 1)
+        # peers.each_with_index { |sibling, index| sibling.update(seq: index + 2, prev_id: ) }
+      end
+    end
+
+    # 构建链 self.prev_id = prev.try(:id)
+    def build_stack
+      if peers.any?
+        # 有同辈节点
+        if peers.count > 1
+
+        else
+          self.seq = peers.count + 1
+        end
+        # update(prev_id: siblings.last.id)
+      else
+        # 无同辈节点
+        self.seq = 1
+      end
+    end
+
+    # 删除栈中: after.prev_id = prev.id; 删除栈首: after.prev_id = nil; 删除栈尾: 无需处理
+    def rebuild_stack
+      prev && after ? after.update(prev_id: prev.id) : after&.update(prev_id: nil)
+    end
+
+    # 插入栈中: ; 插入栈首: ; 插入栈尾: seq = peers.size
+    def update_seq
+      update(seq: peers.size) if superior
+    end
+
+    # 删除栈中: ; 删除栈首: ; 删除栈尾: 无需处理
+    def rebuild_seq
+      # update stack nodes seq
+    end
+end
---
+++ test/models/course_test.rb
@@ -0,0 +1,112 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class CourseTest < ActiveSupport::TestCase
+  setup do
+    @superior = create(:course)
+    @courses = build_list(:course, 4, superior: @superior)
+    @first = @courses.first
+    @second = @courses.second
+    @third = @courses.third
+    @fourth = @courses.fourth
+  end
+
+  test 'create head of stack' do
+    assert_difference('Course.count', +1) do
+      @first.save
+    end
+    assert_equal 1, @first.seq
+    assert_equal @superior.id, @first.superior_id
+  end
+
+  test 'create end of stack' do
+    @first.save
+    assert_difference('Course.count', +1) do
+      @second.save
+    end
+    assert_equal 2, @second.seq
+    assert_equal @first.superior_id, @second.superior_id
+  end
+
+  test 'create middle of stack' do
+    @first.save
+    @second.save
+    @third.seq = 1
+
+    assert_difference('Course.count', +1) do
+      @third.save
+    end
+    assert_equal 2, @second.seq
+    assert_equal 3, @third.seq
+    assert_equal @second.id, @third.prev_id
+    assert_equal 3, @superior.juniors.count
+  end
+
+  test 'create head of another stack' do
+    @first.save
+    @second.save
+
+    @third.superior_id = nil
+    assert_difference('Course.count', +1) do
+      @third.save
+    end
+    assert_nil @third.prev_id
+    assert_nil @third.superior_id
+    assert_nil @superior.superior_id
+    # @superior.seq = 1, @third.seq = 2
+    assert_equal 2, @third.seq
+    # @superior.count + @courses.count = 4
+    assert_equal 4, Course.count
+  end
+
+  # test 'append to the end of stack' do
+  #   @first.save
+  #   assert_difference('Course.count', +1) do
+  #     @second.save
+  #   end
+  #   assert_equal @first.seq, 1
+  #   assert_equal @second.seq, 2
+  #   assert_equal @second.prev_id, @first.id
+  #   assert_equal @second.superior_id, @first.superior_id
+  #   assert_equal @superior.juniors.count, 2
+  # end
+
+  # test 'append to the middle of stack' do
+  #   @first.save
+  #   @second.save
+  #   binding.pry
+  #   assert_difference('Course.count', +1) do
+  #     @third.prev_id = @first.id
+  #     @third.save
+  #   end
+  #   assert_equal @third.prev_id, @first.id
+  #   assert_equal @third.superior_id, @first.superior_id
+  #   assert_equal @second.prev_id, @third.id
+  # end
+end
+
+# == Schema Information
+#
+# Table name: courses
+#
+#  code          :string
+#  name          :string
+#  status        :integer          default("enabled")
+#  mold          :integer          default("private")
+#  seq           :integer          default(0)
+#  juniors_count :integer          default(0)
+#  intro         :text
+#  desc          :text
+#  superior_id   :bigint
+#  prev_id       :bigint
+#  creator_id    :bigint
+#
+# Indexes
+#
+#  index_courses_on_creator_id            (creator_id)
+#  index_courses_on_prev_id               (prev_id)
+#  index_courses_on_superior_id           (superior_id)
+#  index_courses_on_superior_id_and_code  (superior_id,code) UNIQUE
+#  index_courses_on_superior_id_and_name  (superior_id,name) UNIQUE
+#
